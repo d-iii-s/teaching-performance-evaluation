98ed5f0de531ea1775276a670929746049f76cd3
Petr Tuma <petr.tuma@d3s.mff.cuni.cz>
2020-04-22 12:19:34 +0200

    Different invalid stack free example to work around GCC safety

--- src/experiment-memory-corruption/invalid-free-stack.c
+++ src/experiment-memory-corruption/invalid-free-stack.c
@@ -2,22 +2,10 @@
 
 #define SIZE 123
 
-int *function (void) {
-
-    // Allocate an array on stack.
-    int array [SIZE];
-
-    // Write into the array.
-    array [0] = 0;
-
-    // Return array address.
-    return (array);
-}
-
 int main (void) {
 
     // Allocate an array on stack.
-    int *array = function ();
+    int array [SIZE];
 
     // Release the array.
     free (array);
